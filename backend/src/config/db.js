b8c96e274176fd7391523e3daa7150b8c3e625fc
Bruno Bertelli Rabelo <brunob_rabelo@outlook.com>
2026-04-01 23:23:00 -0300

    host db

--- backend/src/config/db.js
+++ backend/src/config/db.js
@@ -9,6 +9,10 @@ const pool = mysql.createPool({
     user: process.env.DB_USER,
     password: process.env.DB_PASS,
     database: process.env.DB_NAME,
+    ssl: {
+        minVersion: 'TLSv1.2',
+        rejectUnauthorized: true
+    },
     waitForConnections: true,
     connectionLimit: 10,
     queueLimit: 0
