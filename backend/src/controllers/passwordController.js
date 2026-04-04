bd17c6b6cb890b8701dc3ad6db7f89a7d0c6903d
Bruno Bertelli Rabelo <brunob_rabelo@outlook.com>
2026-04-03 21:46:23 -0300

    att recuperação de senha

--- backend/src/controllers/passwordController.js
+++ backend/src/controllers/passwordController.js
@@ -29,7 +29,9 @@ const solicitarRecuperacao = async (req, res) => {
 
         // ================= CONFIGURAÇÃO DO E-MAIL REAL (GMAIL) =================
         let transporter = nodemailer.createTransport({
-            service: 'gmail',
+            host: 'smtp.gmail.com',
+            port: 465,
+            secure: true, // true para a porta 465
             auth: { 
                 user: process.env.EMAIL_USER, 
                 pass: process.env.EMAIL_PASS 
